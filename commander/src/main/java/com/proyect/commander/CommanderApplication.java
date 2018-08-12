30e3c8289c05c298727443557498ba6579c9690d
MEMORY SISTEMAS <MEMORY SISTEMAS@192.168.1.42>
2018-08-12 22:42:22 +0200

    INICIAL

---
+++ commander/src/main/java/com/proyect/commander/CommanderApplication.java
@@ -0,0 +1,12 @@
+package com.proyect.commander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommanderApplication.class, args);
+	}
+}
